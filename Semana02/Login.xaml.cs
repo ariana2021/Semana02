932c9a720bc09a1dfbc7fe7041b8e52b1414bb17
Alumno <Alumno@LAB1504-04>
2024-03-23 16:12:41 -0500

    Agregar archivos de proyecto.

---
+++ Semana02/Login.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Semana02
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string usuario = txtUsername.Text;
+            string contraseña = txtPassword.Password;
+
+
+            bool credencialesCorrectas = VerificarCredenciales(usuario, contraseña);
+
+            if (credencialesCorrectas)
+            {
+                MessageBox.Show("Inicio de sesión exitoso");
+
+  
+                Menu menu = new Menu();
+                menu.Show();
+                this.Close(); 
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrectos");
+            }
+        }
+
+        private bool VerificarCredenciales(string usuario, string contraseña)
+        {
+            // Simulación de verificación de credenciales
+            return usuario == "usuario" && contraseña == "123456";
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
